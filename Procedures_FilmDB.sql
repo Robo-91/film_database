1d0276257d8d7b727c6785fbf1e6ddf75363e0b5
Robert Palmer <r91palmer@gmail.com>
2021-04-06 16:16:05 -0500

    Set up procedure for adding new movie

--- Procedures_FilmDB.sql
+++ Procedures_FilmDB.sql
@@ -39,7 +39,30 @@ begin
 end
 
 create procedure proc_AddMovie
-()
+(
+	@newTitle varchar(50),
+	@newYear int,
+	@newGenre varchar(50),
+	@newDuration int,
+	@newCountry varchar(50),
+	@newDirector varchar(50),
+	@newActors varchar(100),
+	@newAvgVote decimal(4,2),
+	@votes int
+)
 as
 begin
+	begin try
+		begin transaction
+			-- if year already exists, grab the id and insert it into the year column
+			-- if genre already exists, grab the id of that genre and insert it into the genre column
+			-- if country already exists, grab the id and insert it into the country column
+			-- if director already exists, grad id and insert it into the director column
+			-- else for each column, insert the new value into the field and then grab that new id and insert into the movie table
+		commit transaction
+	end try
+	begin catch
+		rollback transaction
+		print 'Error inserting Movie'
+	end catch
 end
